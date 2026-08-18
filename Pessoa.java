ab3bdc9d6a51320376324cea11b7ce0235aa3891
Ibrahimovic09 <ibrahimovic.santos@escolar.ifrn.edu.br>
2026-08-18 17:17:07 -0300

    Atividade

---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+    public static void main(String[] args) {
+        String nome = "Ibrahi";
+        int idade = 17;
+        String cidade = "Santa Cruz RN"
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Cidade: " + cidade);
+    }
+}
